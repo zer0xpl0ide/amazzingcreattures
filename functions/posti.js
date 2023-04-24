17ca0b84dd190c2d3873980697f78c5e40ce7f2e
santespro2 <113103637+santespro2@users.noreply.github.com>
2023-04-24 16:22:17 +0200

    Update posti.js

--- functions/posti.js
+++ functions/posti.js
@@ -7,7 +7,7 @@ let ref=event.headers.referer;
       statusCode: 301,
       headers: {
         'cache-control': 'public, max-age=0, must-revalidate',
-        location: 'https://lifeanimalsblog.com/' + pathName[4] 
+        location: 'https://storycenter.net/' + pathName[4] 
       }
     }
   } else {
